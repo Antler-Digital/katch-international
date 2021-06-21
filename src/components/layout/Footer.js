c6422be97fa57d8833df56b84de3bb7fe3d0d546
loyd0 <samloyd@hotmail.co.uk>
2021-06-21 12:18:27 +0100

    added in new addresses

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SocialIcons } from '../elements/SocialIcons'
 import Linked from '../elements/Linked';
-import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faInstagram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 export const blackButtonClassName = "inline-block my-2 bg-primary px-4 text-white text-sm py-2 font-thin rounded hover:text-secondary hover:bg-white transition-bg duration-500"
@@ -30,13 +30,14 @@ const Footer = props => {
 
                 <div className="col-span-1 h-full w-full">
                     <h4
-                        className="mb-4  font-medium px-0 pt-0 pb-2 m-0 text-lg  leading-4 text-gray-800"
+                        className="mb-4  font-medium px-0 pt-0 pb-2 m-0 text-lg  leading-4 text-primary"
                     >
                         WE LIKE TO BE FOLLOWED
                 </h4>
                     <SocialIcons
                         className="text-white space-y-2 mb-4 "
                         showText
+                        itemClassName="hover:text-primary"
                         icons={[
                             {
                                 icon: faFacebookF,
@@ -45,13 +46,18 @@ const Footer = props => {
                             },
                             {
                                 icon: faTwitter,
-                                linkTo: "https://twitter.com/katchbabs",
+                                linkTo: "https://twitter.com/katchintl",
                                 text: "Follow our Twitter"
                             },
                             {
                                 icon: faInstagram,
                                 linkTo: "https://www.instagram.com/katch_int/",
                                 text: "Follow our Instagram"
+                            },
+                            {
+                                icon: faLinkedin,
+                                linkTo: "https://www.linkedin.com/company/katch-international/",
+                                text: "Follow our LinkedIn"
                             },
                         ]} />
 
@@ -61,7 +67,7 @@ const Footer = props => {
                 <div className="col-span-1 md:col-span-2">
                     <div className="flex flex-col sm:flex-row sm:space-x-12 space-y-6 sm:space-y-0 ">
                         <div className="flex flex-col justify-center">
-                            <h4>Dubai Branch</h4>
+                            <h4 className="text-primary">Dubai Branch</h4>
                             <div className="text-sm text-white space-y-1">
                                 <p >Address 2004, Tameem House,</p>
                                 <p>Barsha Heights, P.O.Box 122321</p>
@@ -71,13 +77,13 @@ const Footer = props => {
                             </div>
                         </div>
                         <div className="flex flex-col justify-center">
-                            <h4>London Branch</h4>
+                            <h4 className="text-primary">London Branch</h4>
                             <div className="text-sm text-white space-y-1">
                                 <p >9 Compass House, 5 Park Street</p>
                                 <p> London, SW6 2FB </p>
                                 <p>TEL: +44 20 8895 6383</p>
                                 <p>Email: <a className="hover:text-primary" href="info@katchthis.com" >info@katchthis.com</a></p>
-                                <p>Website: <Linked linkTo="www.katchcommunications.com" >www.katchcommunications.com</Linked></p>
+                                <p>Website: <Linked className="hover:text-primary" linkTo="www.katchcommunications.com" >www.katchcommunications.com</Linked></p>
                             </div>
 
                         </div>
@@ -89,7 +95,7 @@ const Footer = props => {
                 </div>
                 <div className="max-w-lg col-span-3 lg:col-span-1 pt-8 lg:pt-0">
                     <h4
-                        className="mb-4  font-medium px-0 pt-0 pb-2 m-0 text-lg  leading-4 text-gray-800"
+                        className="mb-4  font-medium px-0 pt-0 pb-2 m-0 text-lg  leading-4 text-primary"
                     >
                         KATCH US
                     </h4>
