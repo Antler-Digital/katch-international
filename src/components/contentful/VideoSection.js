469223e6cf5c03812f387ad0f48587d0db669267
jlsfurness@gmail.com <jlsfurness@gmail.com>
2022-07-27 09:23:02 +0100

    Updated video component to autoplay on safari

--- src/components/contentful/VideoSection.js
+++ src/components/contentful/VideoSection.js
@@ -6,6 +6,20 @@ import Video from "./Video"
 
 const VideoSection = ({ name, type, videoId, video, placeholder }) => {
   const containerRef = React.useRef(null)
+  const [isVerticalVideo, setIsVerticalVideo] = React.useState(false)
+
+  const handleResize = () => {
+    const y = window.innerHeight
+    const x = window.innerWidth
+    setIsVerticalVideo(y > x)
+  }
+
+  console.log(isVerticalVideo)
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize)
+  }, [])
 
   useSnapToNext(containerRef)
 
@@ -15,12 +29,20 @@ const VideoSection = ({ name, type, videoId, video, placeholder }) => {
     ) : (
       <LocalVideo />
     )
+
+  const VerticalVideoComponent = () =>
+    type === "youtube" ? (
+      <Video videoSrcURL={`https://www.youtube.com/embed/${videoId}`} />
+    ) : (
+      <LocalVideo />
+    )
+
   return (
     <section
       ref={containerRef}
       className={`h-screen w-full bg-primary flex items-center justify-center`}
     >
-      <VideoComponent />
+      {isVerticalVideo ? <VerticalVideoComponent /> : <VideoComponent />}
       <ul className="text-white flex space-x-1 relative z-0">
         <li className="border border-white w-2 h-2 rounded-full animate-bounce-big delay-1s"></li>
         <li className="border border-white w-2 h-2 rounded-full animate-bounce-big delay-2s"></li>
