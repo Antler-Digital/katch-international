82a500832b98772fec9eaab840a71cab8b283af2
loyd0 <samloyd@hotmail.co.uk>
2022-08-11 12:37:25 +0100

    WIP: Case study header

--- src/components/contentful/HeroSection.js
+++ src/components/contentful/HeroSection.js
@@ -24,105 +24,115 @@ function HeroSection({
 
   const title = header.trim().split(" ")
 
+  const getTextStyle = (text) => {
+    const textLength = text?.length || 0;
+    console.log(textLength)
 
-  return (
-    <>
-      <section
-        className={`${backgroundImage || carouselImages
-          ? "h-[500px]"
-          : "min-h-[300px] md:min-h-[400px]"
-          }  relative ${(centerHeading || !showForm) && "flex items-center"}`}
+    if (textLength > 40) return "-bottom-[53px] md:-bottom-[63px] lg:-bottom-[140px]  relative text-4xl md:text-5xl lg:text-[80px]"
+    if (textLength > 30) return "-bottom-[42px] md:-bottom-[63px] lg:-bottom-[176px]  relative text-5xl md:text-7xl lg:text-[110px]"
+    return "-bottom-[68px] md:-bottom-[81px] lg:-bottom-[179px]  relative text-6xl md:text-7xl lg:text-[160px]"
+
+}
+
+
+return (
+  <>
+    <section
+      className={` h-[500px] ${backgroundImage || carouselImages
+        ? "h-[500px]"
+        : "-min-h-[300px] lmd:min-h-[400px]"
+        }  relative ${(centerHeading || !showForm) && "flex items-center"}`}
+    >
+      <div
+        className={`max-w-screen-2xl mx-auto  w-full h-full flex flex-wrap relative px-4 z-20 self-end`}
       >
-        <div
-          className={`max-w-screen-2xl mx-auto  w-full h-full flex flex-wrap relative px-4 z-20`}
-        >
-          <div className={` w-full md:h-full text-left ${colorText}`}>
-            {header && (
-              <h1
-                className={`-bottom-[68px] md:-bottom-[81px] lg:-bottom-[179px] absolute text-6xl md:text-7xl lg:text-[160px] font-bold block`}
-              >
+        {/* <div className={` w-full md:h-full text-left relative bottom-0 `}> */}
+          {header && (
+            <h1
+              className={`${getTextStyle(header?.trim())} font-bold block self-end ${colorText}`}
+            >
 
-                <span>{title[0]}</span>
-                <span className="block last:text-black">{
-                  title.slice(1, title.length).join(' ')
-                }
-                </span>
-              </h1>
-            )}
-          </div>
+              <span>{title[0]}</span>
+              <span className="block last:text-black">{
+                title.slice(1, title.length).join(' ')
+              }
+              </span>
+            </h1>
+          )}
         </div>
-        <div className="h-full absolute top-0 z-10 w-full sm:px-4 bg-black bg-opacity-25" />
-        {backgroundImage && !carouselImages && (
-          <GatsbyImage
-            className="absolute h-full w-full top-0 "
-            style={{ position: "absolute" }}
-            image={backgroundImage.gatsbyImageData}
-            alt={backgroundImage.title}
-          />
-        )}
+      {/* </div> */}
+      <div className="h-full absolute top-0 z-10 w-full sm:px-4 bg-black bg-opacity-25" />
+      {backgroundImage && !carouselImages && (
+        <GatsbyImage
+          className="absolute h-full w-full top-0 "
+          style={{ position: "absolute" }}
+          image={backgroundImage.gatsbyImageData}
+          alt={backgroundImage.title}
+        />
+      )}
 
-        {(carouselImages || backgroundImage) && (
-          <div className="absolute h-full w-full top-0 z-10">
-            {carouselImages && !backgroundImage && (
-              <Carousel
-                settings={{
-                  prevArrow: false,
-                  nextArrow: false,
-                  autoplay: true,
-                  arrows: false,
-                }}
-                className="w-full"
-              >
-                {carouselImages &&
-                  carouselImages.map((image) => (
-                    <GatsbyImage
-                      className="h-full w-full mx-auto top-0"
-                      style={{ display: "absolute" }}
-                      image={image.gatsbyImageData}
-                      alt={image.title}
-                    />
-                  ))}
-              </Carousel>
-            )}
-
-            {carouselImages && backgroundImage && (
-              <div className="absolute h-full w-full top-0 flex flex-col md:flex-row justify-between">
-                <div className="md:w-3/5 w-full">
+      {(carouselImages || backgroundImage) && (
+        <div className="absolute h-full w-full top-0 z-10">
+          {carouselImages && !backgroundImage && (
+            <Carousel
+              settings={{
+                prevArrow: false,
+                nextArrow: false,
+                autoplay: true,
+                arrows: false,
+              }}
+              className="w-full"
+            >
+              {carouselImages &&
+                carouselImages.map((image) => (
                   <GatsbyImage
-                    className="md:absolute h-[250px] md:h-full w-full top-0"
-                    style={{ position: "block" }}
-                    image={backgroundImage.gatsbyImageData}
-                    alt={backgroundImage.title}
+                    className="h-full w-full mx-auto top-0"
+                    style={{ display: "absolute" }}
+                    image={image.gatsbyImageData}
+                    alt={image.title}
                   />
-                </div>
-                <div className="w-full md:w-2/5 h-full">
-                  <Carousel
-                    settings={{
-                      prevArrow: false,
-                      nextArrow: false,
-                      autoplay: true,
-                      arrows: false,
-                    }}
-                    className="w-full h-full"
-                  >
-                    {carouselImages &&
-                      carouselImages.map((image) => (
-                        <GatsbyImage
-                          className="h-[250px] md:h-[500px] w-full mx-auto md:top-0 object-cover"
-                          style={{ display: "block" }}
-                          image={image.gatsbyImageData}
-                          alt={image.title}
-                        />
-                      ))}
-                  </Carousel>
-                </div>
+                ))}
+            </Carousel>
+          )}
+
+          {carouselImages && backgroundImage && (
+            <div className="absolute h-full w-full top-0 flex flex-col md:flex-row justify-between">
+              <div className="md:w-3/5 w-full">
+                <GatsbyImage
+                  className="md:absolute h-[250px] md:h-full w-full top-0"
+                  style={{ position: "block" }}
+                  image={backgroundImage.gatsbyImageData}
+                  alt={backgroundImage.title}
+                />
               </div>
-            )}
-          </div>
-        )}
-      </section>
-    </>
-  )
+              <div className="w-full md:w-2/5 h-full">
+                <Carousel
+                  settings={{
+                    prevArrow: false,
+                    nextArrow: false,
+                    autoplay: true,
+                    arrows: false,
+                  }}
+                  className="w-full h-full"
+                >
+                  {carouselImages &&
+                    carouselImages.map((image) => (
+                      <GatsbyImage
+                        className="h-[250px] md:h-[500px] w-full mx-auto md:top-0 object-cover"
+                        style={{ display: "block" }}
+                        image={image.gatsbyImageData}
+                        alt={image.title}
+                      />
+                    ))}
+                </Carousel>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </section>
+  </>
+)
 }
 
 export default HeroSection
