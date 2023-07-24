c9e890fc57eb5b7baf0ea2a372455705f75cc608
jlsfurness@gmail.com <jlsfurness@gmail.com>
2023-07-24 10:40:02 +0100

    Removed double h1

--- src/pages/{ContentfulPage.slug}.js
+++ src/pages/{ContentfulPage.slug}.js
@@ -137,7 +137,6 @@ const PageTemplate = ({ data: { contentfulPage }, location }) => {
         path={location.pathname}
       />
       <WhatsappIcon path={location.pathname} />
-      {metaTitle && <h1 className="absolute opacity-0">{metaTitle}</h1>}
 
       <div>
         {snapSections &&
